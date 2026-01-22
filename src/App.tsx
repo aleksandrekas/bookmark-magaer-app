61ac273bbf7b1678d94df3b407744a51d3c42ef0
aleksandre <aleksandrekasrashvili@gmail.com>
2026-01-23 00:06:52 +0400

    started with sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Login from "./assets/components/log/Login"
 import LinkItem from "./assets/components/linkItem/LinkItem"
 import Header from "./assets/components/navbar/Header"
+import SideBar from "./assets/components/sideBar/Sidebar"
 function App() {
 
   return (
-    <>
+    <main>
+      <SideBar/>
       <Header />
-    </>
+    </main>
   )
 }
 
